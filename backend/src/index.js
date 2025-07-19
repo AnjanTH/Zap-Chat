ea72444a7a4e559344b2f9af8b92a64e97f03659
AnjanTH <anjanth2552004@gmail.com>
2025-07-19 20:30:28 +0530

    ready for hosting

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,8 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://zapchat18.vercel.app"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
